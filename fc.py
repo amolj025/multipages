8c793c9857738832ff4b5b0bc4ef2eb9576e59e2
amolj025 <111583494+amolj025@users.noreply.github.com>
2022-08-20 01:10:10 +0530

    Update fc.py

--- fc.py
+++ fc.py
@@ -21,14 +21,14 @@ from pytesseract import Output
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #img = Image.open('20220711122009597_1_1_1.jpg')
 s = '20220711183010711_1_1_3.jpg'
-s1 = 'C:\\Users\\Amol\\Desktop\\streamlit-examples\\multipages'
-s2 = '\\';
-s3 = s1 + s2 + s; 
-print(s3);
-st.write(s3)
+#s1 = 'C:\\Users\\Amol\\Desktop\\streamlit-examples\\multipages'
+#s2 = '\\';
+#s3 = s1 + s2 + s; 
+#print(s3);
+st.write(s)
 #st.write(s3)
 #img = Image.open(r'C:\Users\Amol\Desktop\streamlit-examples\20220711183010711_1_1_3.jpg')
-img = Image.open(s3)
+img = Image.open(s)
 st.image(img)
 img.load()
 a = pytesseract.image_to_string(img, lang='eng')#'eng' for english
